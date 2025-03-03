851c3a5d35972c6ed83f0719576ea8840632475d
Bakos Gergő <99805027+BakosGergo02@users.noreply.github.com>
2025-03-03 23:42:40 +0100

    Create 3A.py

---
+++ Raspberry_PI/Homeworks/3.het/3A.py
@@ -0,0 +1,20 @@
+def caesar(msg,key):
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    new_message = ""
+    
+    for c in msg:
+        position = alphabet.find(c)
+        new_position = (position + key) % len(alphabet)
+        new_character = alphabet[new_position]
+        new_message += new_character
+        
+    return new_message
+
+key = 3
+while True:
+    message = input("Please enter a message:").lower()
+    if message == 'q': break
+    encry_message = caesar(message,key)
+    decry_message = caesar(encry_message,-key)
+    print("Encrypted message:", encry_message)
+    print("Decrypted message:", decry_message)
